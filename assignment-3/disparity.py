faebd5fb2ee046e3c317f37c79ca75f82dfe7fab
Mostafa Abdullah <mostafaabdullahahmed@gmail.com>
2017-12-14 21:29:44 +0200

    Fix upper bounds

--- assignment-3/disparity.py
+++ assignment-3/disparity.py
@@ -7,8 +7,8 @@ RESULT_FOLDER = "result/disparity"
 
 def calculate_SAD(img1, img2, pos1, pos2, window):
     res = 0
-    for addi in range(-window // 2, window // 2):
-        for addj in range(-window // 2, window // 2):
+    for addi in range(-window // 2, window // 2 + 1):
+        for addj in range(-window // 2, window // 2 + 1):
             if pos1[0] + addi < 0 or pos1[1] + addj < 0:
                 continue
             if pos2[0] + addi < 0 or pos2[1] + addj < 0:
@@ -28,17 +28,15 @@ def estimate_disparity(img1, img2, window, disparity_range):
     res = np.zeros(img1.shape)
     for i in range(len(img1)):
         for j in range(len(img1[i])):
-            min_val = (window + 1) * (window + 1) * 255
-            for i2 in range(i + disparity_range[0], i + disparity_range[1]):
-                for j2 in range(j + disparity_range[2], j + disparity_range[3]):
+            min_val = (window + 1) * (window + 1) * 256
+            for i2 in range(max(0, i + disparity_range[0]), i + disparity_range[1] + 1):
+                for j2 in range(max(0, j + disparity_range[2]), j + disparity_range[3] + 1):
                     SAD = calculate_SAD(img1, img2, (i, j), (i2, j2), window)
-
                     min_val = min(min_val, SAD)
 
             res[i][j] = min_val
-
+    res = res / res.max() * 255
     return res
-
 
 def write_disparity(img_name):
     print("START {}".format(img_name))
@@ -52,4 +50,3 @@ def write_disparity(img_name):
 
 write_disparity("cones")
 write_disparity("teddy")
-
